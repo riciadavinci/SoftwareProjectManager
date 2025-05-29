4f46b06a823e96e20a0f545345777e4f5e4a584b
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-05-29 15:29:47 +0200

    Add 'SoftwareProject; ORM class

---
+++ software_project_manager/models/SoftwareProject.py
@@ -0,0 +1,28 @@
+from software_project_manager import db
+from datetime import datetime
+
+class SoftwareProject(db.Model):
+    __tablename__ = "software_projects"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(250), nullable=False)
+    desciption = db.Column(db.String(2500), nullable=True)
+    created_on = db.Column(db.DateTime, default=datetime.now())
+    modified_on = db.Column(db.DateTime, default=datetime.now())
+    tasks = db.relationship("Task", backref="task_id")
+
+    def __init__(self, name, description=None):
+        self.name = name
+        self.desciption = description
+    
+    def __repr__(self):
+        return f"SoftwareProject <{self.id}>"
+
+    def to_dict(self):
+        data = {
+            "id": self.id,
+            "name": self.name,
+            "description": self.desciption,
+            "created_on": self.created_on,
+            "modified_on": self.modified_on
+        }
+        return data
