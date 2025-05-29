0b29839766127b258502c3aefbcde095c81f7432
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-05-29 15:31:10 +0200

    Add database URI and other imports

--- software_project_manager/__init__.py
+++ software_project_manager/__init__.py
@@ -1,8 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_restful import Api
 
 app = Flask(__name__)
-# db = SQLAlchemy(app)
 
-from software_project_manager import views
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///software_project_manager.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+api = Api(app)
+# db.init_app()
+
+# from models import SoftwareProject
+from software_project_manager import models
 from software_project_manager import apis
+from software_project_manager import views
+from software_project_manager import cli_commands
