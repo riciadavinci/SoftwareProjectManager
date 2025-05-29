aa33c0565e2aff9dfb57c5b5a8290ca897c7e701
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-05-29 15:30:37 +0200

    Fix imports

--- software_project_manager/models/__init__.py
+++ software_project_manager/models/__init__.py
@@ -1,1 +1,7 @@
-from software_project_manager import db
+# from software_project_manager import db
+
+# from .SoftwareProject import SoftwareProject
+
+from .SoftwareProject import SoftwareProject
+from .TaskStatus import TaskStatus
+from .Task import Task
