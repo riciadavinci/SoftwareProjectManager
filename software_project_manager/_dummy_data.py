3c75271d1cbed924dd92874a2966cc25ae727c0f
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-05-29 19:51:52 +0200

    Add dummy Tasks

--- software_project_manager/_dummy_data.py
+++ software_project_manager/_dummy_data.py
@@ -22,3 +22,48 @@ DUMMY_SOFTWARE_PROJECTS = [
         "description": "Description of Test Project 5"
     },
 ]
+
+DUMMY_TASKS = [
+    {
+        "title": "Sample Task 1",
+        "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores ",
+        "task_status_id": "todo",
+        "software_project_id": 1
+    },
+    {
+        "title": "Sample Task 2",
+        "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores ",
+        "task_status_id": "todo",
+        "software_project_id": 4
+    },
+    {
+        "title": "Sample Task 3",
+        "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores ",
+        "task_status_id": "todo",
+        "software_project_id": 2
+    },
+    {
+        "title": "Sample Task 4",
+        "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores ",
+        "task_status_id": "todo",
+        "software_project_id": 1
+    },
+    {
+        "title": "Sample Task 5",
+        "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores ",
+        "task_status_id": "todo",
+        "software_project_id": 3
+    },
+    {
+        "title": "Sample Task 6",
+        "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores ",
+        "task_status_id": "todo",
+        "software_project_id": 5
+    },
+    {
+        "title": "Sample Task 7",
+        "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores ",
+        "task_status_id": "todo",
+        "software_project_id": 4
+    }
+]
