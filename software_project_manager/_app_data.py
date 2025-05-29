2019366a36ae0b83db63deb7aeebe1a654cb828c
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-05-29 19:52:18 +0200

    Add Default Reference Types

--- software_project_manager/_app_data.py
+++ software_project_manager/_app_data.py
@@ -4,3 +4,24 @@ DEFAULT_TASK_STATUSES = {
     "wip": "W.I.P",
     "done": "Done"
 }
+
+DEFAULT_REFERENCE_TYPES = [
+    {
+        "name": "Youtube Video"
+    },
+    {
+        "name": "Online Article"
+    },
+    {
+        "name": "Online Documentation"
+    },
+    {
+        "name": "Book"
+    },
+    {
+        "name": "Research Paper"
+    },
+    {
+        "name": "Online Video"
+    }
+]
