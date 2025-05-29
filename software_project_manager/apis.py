16788179ff07956960672bef3b13e164076777a0
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-05-29 18:13:12 +0200

    Implement Tasks get and insert; Fix minor bugs;

--- software_project_manager/apis.py
+++ software_project_manager/apis.py
@@ -1,11 +1,17 @@
 from flask_restful import Resource, reqparse
 from flask import jsonify, make_response, request
+from sqlalchemy.orm.exc import UnmappedInstanceError
 
 from software_project_manager import db
 from software_project_manager import api
 from software_project_manager.models import TaskStatus
 from software_project_manager.models import SoftwareProject
 from software_project_manager.models import Task
+
+
+# ----------------------------------------
+# 'TaskStatus' API:
+# ----------------------------------------
 
 class TaskStatusResource(Resource):
     def get(self, id):
@@ -26,9 +32,13 @@ class TaskStatusListResource(Resource):
         except Exception as ex:
             return make_response(jsonify({"error_message": str(ex)}), 501)
 
-api.add_resource(TaskStatusResource, "/task-status/<string:id>")
-api.add_resource(TaskStatusListResource, "/task-status/")
+api.add_resource(TaskStatusResource, "/api/task-status/<string:id>")
+api.add_resource(TaskStatusListResource, "/api/task-status/")
 
+
+# ----------------------------------------
+# 'SoftwareProject' API:
+# ----------------------------------------
 
 class SoftwareProjectResource(Resource):
     def get(self, id):
@@ -49,12 +59,14 @@ class SoftwareProjectResource(Resource):
             db.session.delete(software_project)
             db.session.commit()            
             return make_response(jsonify({"message": f"Deleted {software_project}"}))
+        except UnmappedInstanceError as ex:
+                return make_response(jsonify({"error_message": f"SoftwareProject <{id}> not found! Therefore, cannot delete it."}), 404)
         except Exception as ex:
             return make_response(jsonify({"error_message": str(ex)}), 501)
 
 class SoftwareProjectListResource(Resource):
     def get(self):
-        data = {}
+        data = []
         try:
             data = [software_project_item.to_dict() for software_project_item in SoftwareProject.query.all()]
             return make_response(jsonify({"data": data}), 200)
@@ -74,5 +86,53 @@ class SoftwareProjectListResource(Resource):
         except Exception as ex:
             return make_response(jsonify({"error_message": str(ex)}), 501)
 
-api.add_resource(SoftwareProjectResource, "/software-project/<int:id>")
-api.add_resource(SoftwareProjectListResource, "/software-project/")
+api.add_resource(SoftwareProjectResource, "/api/software-project/<int:id>")
+api.add_resource(SoftwareProjectListResource, "/api/software-project/")
+
+
+# ----------------------------------------
+# 'Task' API:
+# ----------------------------------------
+
+class TaskResource(Resource):
+    def get(self, id):
+        pass
+
+    def delete(self, id):
+        pass
+
+class TaskListResource(Resource):
+    def get(self):
+        data = []
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument("swpr_id", required=False, type=int)
+            parser.add_argument("user_id", required=False, type=int)
+            args = parser.parse_args()
+            if (args["swpr_id"]):
+                data = [task_item.to_dict() for task_item in Task.query.filter_by(software_project_id=args.swpr_id)]
+            else:
+                data = [task_item.to_dict() for task_item in Task.query.all()]
+            return make_response(jsonify({"data": data}), 200)
+        except Exception as ex:
+            return make_response(jsonify({"error_message": str(ex)}), 501)
+    
+    def post(self):
+        data = []
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument("title", type=str)
+            parser.add_argument("description", type=str)
+            parser.add_argument("task_status_id", type=str)
+            parser.add_argument("software_project_id", type=int)
+            args = parser.parse_args()
+            task = Task(args.title, args.description, args.task_status_id, args.software_project_id)
+            db.session.add(task)
+            db.session.commit()
+            return make_response(jsonify({"data": task.to_dict()}), 200)
+        except Exception as ex:
+            return make_response(jsonify({"error_message": str(ex)}), 501)
+
+
+api.add_resource(TaskResource, "/api/task/<int:id>")
+api.add_resource(TaskListResource, "/api/task/")
