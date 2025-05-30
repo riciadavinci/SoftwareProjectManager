395aeb4c1fa3ebcd0f977eac2c06cc64acdd9424
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-05-30 21:20:37 +0200

    Implement Update method; Add cascade deletion

--- software_project_manager/models/SoftwareProject.py
+++ software_project_manager/models/SoftwareProject.py
@@ -8,11 +8,17 @@ class SoftwareProject(db.Model):
     desciption = db.Column(db.String(2500), nullable=True)
     created_on = db.Column(db.DateTime, default=datetime.now())
     modified_on = db.Column(db.DateTime, default=datetime.now())
-    tasks = db.relationship("Task", backref="swpr_task_id")
+    tasks = db.relationship("Task", backref="swpr_task_id", cascade="all, delete")
 
     def __init__(self, name, description=None):
         self.name = name
         self.desciption = description
+    
+    def update(self, new_name, new_description=None):
+        self.name = new_name
+        self.desciption = new_description
+        self.modified_on = datetime.now()
+        db.session.commit()
     
     def __repr__(self):
         return f"SoftwareProject <{self.id}>"
