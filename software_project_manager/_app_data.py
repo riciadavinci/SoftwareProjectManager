92971eabbd33c864d247c4e7a8bffc9f0ec1a023
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-06-09 21:00:40 +0200

    Add default projects & project references; 1st project is this repo itself

--- software_project_manager/_app_data.py
+++ software_project_manager/_app_data.py
@@ -7,21 +7,111 @@ DEFAULT_TASK_STATUSES = {
 
 DEFAULT_REFERENCE_TYPES = [
     {
-        "name": "Youtube Video"
+        "name": "Youtube Video" # 1
     },
     {
-        "name": "Online Article"
+        "name": "Online Article" # 2
     },
     {
-        "name": "Documentation"
+        "name": "Online Documentation" # 3
     },
     {
-        "name": "Book"
+        "name": "Book" # 4
     },
     {
-        "name": "Research Paper"
+        "name": "Research Paper" # 5
     },
     {
-        "name": "Online Video"
+        "name": "Online Video" # 6
+    },
+    {
+        "name": "YouTube Playlist" # 7
+    }
+]
+
+DEFAULT_SOFTWARE_PROJECTS = [
+    {
+        "name": "Software Project Manager",
+        "description": "Software Project Manager's (itself) description: It became a little meta! :)"
+    }
+]
+
+DEFAULT_PROJECT_REFERENCES = [
+    {
+        "name": "pytest Basics",
+        "reference_type_id": 7,
+        "software_project_id": 1,
+        "url": "https://www.youtube.com/playlist?list=PLxNPSjHT5qvuZ_JT1bknzrS8YqLiMjNpS",
+        "author": None
+    },
+    {
+        "name": "Add Unit Tests to Flask App and API with Pytest",
+        "reference_type_id": 1,
+        "software_project_id": 1,
+        "url": "https://www.youtube.com/watch?v=3N2wm3nIuRE",
+        "author": None
+    },
+    {
+        "name": "Testing Flask Applications with pytest",
+        "reference_type_id": 1,
+        "software_project_id": 1,
+        "url": "https://www.youtube.com/watch?v=OcD52lXq0e8",
+        "author": "Patrick Kennedy"
+    },
+    {
+        "name": "Python Tutorial: Unit Testing Your Code with the unittest Module",
+        "reference_type_id": 1,
+        "software_project_id": 1,
+        "url": "https://www.youtube.com/watch?v=6tNS--WetLI",
+        "author": None
+    },
+    {
+        "name": "Getting Started With Testing in Flask",
+        "reference_type_id": 1,
+        "software_project_id": 1,
+        "url": "https://www.youtube.com/watch?v=RLKW7ZMJOf4",
+        "author": None
+    },
+    {
+        "name": "Flask Restful docs",
+        "reference_type_id": 3,
+        "software_project_id": 1,
+        "url": "https://flask-restful.readthedocs.io/en/latest/",
+        "author": None
+    },
+    {
+        "name": "SQLite FTS5 Extension - Full Text Search",
+        "reference_type_id": 3,
+        "software_project_id": 1,
+        "url": "https://www.sqlite.org/fts5.html",
+        "author": None
+    },
+    {
+        "name": "SQLAlchemy - Cascades",
+        "reference_type_id": 3,
+        "software_project_id": 1,
+        "url": "https://docs.sqlalchemy.org/en/20/orm/cascades.html",
+        "author": None
+    },
+    {
+        "name": "User Authentication in Flask App using JWT [ JSON Web Token ]",
+        "reference_type_id": 1,
+        "software_project_id": 1,
+        "url": "https://www.youtube.com/watch?v=_3NKBHYcpyg",
+        "author": None
+    },
+    {
+        "name": "JWT Authentication for Flask",
+        "reference_type_id": 7,
+        "software_project_id": 1,
+        "url": "https://www.youtube.com/playlist?list=PLEt8Tae2spYmugodsDflw5U8zp1yzSPgU",
+        "author": "Ssali Jonathan"
+    },
+    {
+        "name": "Basic Relationship Patterns - SQLAlchemy",
+        "reference_type_id": 7,
+        "software_project_id": 1,
+        "url": "https://docs.sqlalchemy.org/en/13/orm/basic_relationships.html",
+        "author": None
     }
 ]
