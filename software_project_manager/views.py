24bb60f9fc0183e2bb282a96e97d73c01dc8ea31
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-06-09 18:35:26 +0200

    Render login page (basic view)

--- software_project_manager/views.py
+++ software_project_manager/views.py
@@ -1,7 +1,14 @@
+from flask import render_template
 from software_project_manager import app
 
-@app.route("/")
+
 @app.route("/home")
 @app.route("/index")
+@app.route("/")
 def home_page():
-    return "<h1>Hello, World!</h1>"
+    return render_template("login.html")
+
+@app.route("/login")
+def login_page():
+    
+    return render_template("login.html")
