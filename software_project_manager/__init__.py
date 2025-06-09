caaf42cd39d60928f042ef583f1f6e41c0f36cc7
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-06-09 18:21:51 +0200

    Move App Config to separate file & class - 'Config'

--- software_project_manager/__init__.py
+++ software_project_manager/__init__.py
@@ -2,16 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api
+from software_project_manager.config import Config
 
 app = Flask(__name__)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///software_project_manager.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 api = Api(app)
-# db.init_app()
 
 # from models import SoftwareProject
 from software_project_manager import models
---
+++ software_project_manager/config.py
@@ -0,0 +1,7 @@
+import os
+
+
+class Config:
+    SECRET_KEY = "INf6k1BWQoxH0toXux880wZYtoTLIzbc"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///software_project_manager.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
