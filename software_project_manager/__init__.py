77e1085e380430e37950b40d352421bb3a968087
riciadavinci <rishikesh.nerurkar879@gmail.com>
2025-06-12 17:51:36 +0200

    Create LoginForm HTML & enable Flask Bootstrap

--- software_project_manager/__init__.py
+++ software_project_manager/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api
+from flask_bootstrap import Bootstrap
 from software_project_manager.config import Config
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 api = Api(app)
+Bootstrap(app)
 
 # from models import SoftwareProject
 from software_project_manager import models
